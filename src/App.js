cb0ae4580ea4063f50d8c658e2ffe908a7d1f4bd
pawanbenjamin <pawanbenjamin@gmail.com>
2022-03-10 00:26:19 -0500

    light code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react'
+
+/*
+    GOAL:
+        1. To fetch pokemon from the PokeAPI, 
+        2. Render them all in the Zoo component,
+        3. Have buttons which paginate thorugh the pokemon (via next and previous)
+ */
 
 function App() {
-  return <div style={{ textAlign: "center" }}></div>;
+  return <div></div>
 }
 
-export default App;
+export default App
---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+
+/* 
+    Write a AJAX helper function which GETs and returns pokemon from the pokemon API (url above)
+    (make sure to export it!)
+*/
