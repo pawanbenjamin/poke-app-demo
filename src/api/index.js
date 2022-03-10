227710069b38e22681055b3721c5e283551888be
pawanbenjamin <pawanbenjamin@gmail.com>
2022-03-10 00:42:12 -0500

    goal and roadmap

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 const url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
 
 /* 
-    Write a AJAX helper function which GETs and returns pokemon from the pokemon API (url above)
-    (make sure to export it!)
+    Write a AJAX helper function which takes a url, and returns a list of pokemon 
+        TIPS: use the url above to test it out
 */
